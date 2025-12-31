b0e97b4f2ce4ced5481516bb628acc554019f585
Daniiislo <22120383@student.hcmus.edu.vn>
2025-12-31 08:24:36 +0700

    feat: setup tests app and create test model

--- english_app/settings.py
+++ english_app/settings.py
@@ -16,90 +16,89 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=True)
 
 # Allowed hosts
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])  # comma-separated in .env
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS", default=["localhost", "127.0.0.1"]
+)  # comma-separated in .env
 
 # Application definition
 INSTALLED_APPS = [
     # default django apps
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # third-party apps
-    'corsheaders',
-    'rest_framework',
-    'django_filters',
-    'drf_spectacular',
-    'drf_spectacular_sidecar',
-
+    "corsheaders",
+    "rest_framework",
+    "django_filters",
+    "drf_spectacular",
+    "drf_spectacular_sidecar",
     # authentication apps
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
-
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     # custom apps
-    'accounts',
+    "accounts",
+    "tests",
 ]
 
 # Middleware
 MIDDLEWARE = [
     # default django middleware
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # third-party middleware
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 # Root URL conf
-ROOT_URLCONF = 'english_app.urls'
+ROOT_URLCONF = "english_app.urls"
 
 # Templates
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'english_app.wsgi.application'
+WSGI_APPLICATION = "english_app.wsgi.application"
 
 # Database settings
 DATABASES = {
-    'default': {
-        'ENGINE': env('DB_ENGINE'),
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+    "default": {
+        "ENGINE": env("DB_ENGINE"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
 # Cache settings
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': f"redis://{env('REDIS_HOST')}:{env('REDIS_PORT')}/{env('REDIS_DB')}",
-        'TIMEOUT': 30000,
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{env('REDIS_HOST')}:{env('REDIS_PORT')}/{env('REDIS_DB')}",
+        "TIMEOUT": 30000,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     }
 }
 
@@ -107,24 +106,24 @@ CACHES = {
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
 # Internationalization
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Ho_Chi_Minh'
+TIME_ZONE = "Asia/Ho_Chi_Minh"
 
 USE_I18N = True
 
@@ -132,20 +131,20 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Media files (Images, Videos, etc.)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # email settings
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = env.int('EMAIL_PORT')
-EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
-EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env.int("EMAIL_PORT")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+EMAIL_BACKEND = env("EMAIL_BACKEND")
 
 # CORS
 CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS", default=True)
@@ -153,55 +152,51 @@ CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
 # REST framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'django_filters.rest_framework.DjangoFilterBackend',
-    ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 # Simple jwt settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),  
-    'ROTATE_REFRESH_TOKENS': True,  
-    'BLACKLIST_AFTER_ROTATION': True,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 # OpenAPI/Swagger (drf-spectacular)
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'English App API',
-    'DESCRIPTION': 'API documentation',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-    'COMPONENT_SPLIT_REQUEST': True,
-    'SECURITY_SCHEMES': {
-        'BearerAuth': {
-            'type': 'http',
-            'scheme': 'bearer',
-            'bearerFormat': 'JWT',
+    "TITLE": "English App API",
+    "DESCRIPTION": "API documentation",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "COMPONENT_SPLIT_REQUEST": True,
+    "SECURITY_SCHEMES": {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
         }
     },
-    'SECURITY': [{'BearerAuth': []}],
+    "SECURITY": [{"BearerAuth": []}],
 }
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom User Model
-AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = "accounts.User"
 
 # OAuth2 Settings
-OAUTH2_GOOGLE_KEY = env('OAUTH2_GOOGLE_KEY', default='')
-OAUTH2_GOOGLE_SECRET = env('OAUTH2_GOOGLE_SECRET', default='')
-OAUTH2_GOOGLE_REDIRECT_URI = env('OAUTH2_GOOGLE_REDIRECT_URI', default='')
-OAUTH2_GOOGLE_SCOPE = env('OAUTH2_GOOGLE_SCOPE', default='')
+OAUTH2_GOOGLE_KEY = env("OAUTH2_GOOGLE_KEY", default="")
+OAUTH2_GOOGLE_SECRET = env("OAUTH2_GOOGLE_SECRET", default="")
+OAUTH2_GOOGLE_REDIRECT_URI = env("OAUTH2_GOOGLE_REDIRECT_URI", default="")
+OAUTH2_GOOGLE_SCOPE = env("OAUTH2_GOOGLE_SCOPE", default="")
 
-OAUTH2_FACEBOOK_KEY = env('OAUTH2_FACEBOOK_KEY', default='')
-OAUTH2_FACEBOOK_SECRET = env('OAUTH2_FACEBOOK_SECRET', default='')
-OAUTH2_FACEBOOK_REDIRECT_URI = env('OAUTH2_FACEBOOK_REDIRECT_URI', default='')
-OAUTH2_FACEBOOK_SCOPE = env('OAUTH2_FACEBOOK_SCOPE', default='')
+OAUTH2_FACEBOOK_KEY = env("OAUTH2_FACEBOOK_KEY", default="")
+OAUTH2_FACEBOOK_SECRET = env("OAUTH2_FACEBOOK_SECRET", default="")
+OAUTH2_FACEBOOK_REDIRECT_URI = env("OAUTH2_FACEBOOK_REDIRECT_URI", default="")
+OAUTH2_FACEBOOK_SCOPE = env("OAUTH2_FACEBOOK_SCOPE", default="")
--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+
 
 # Create your models here.
+class Test(models.Model):
+    title = models.CharField(max_length=255)
+    level = models.CharField(
+        max_length=2,
+        choices=[
+            ("B1", "B1"),
+            ("B2", "B2"),
+            ("C1", "C1"),
+            ("C2", "C2"),
+            ("A1", "A1"),
+            ("A2", "A2"),
+        ],
+        default="A1",
+    )
+    skill = models.CharField(
+        max_length=1,
+        choices=[
+            ("R", "Reading"),
+            ("L", "Listening"),
+            ("S", "Speaking"),
+            ("W", "Writing"),
+        ],
+        default="R",
+    )
+    time = models.IntegerField(
+        help_text="Time in minutes", validators=[MinValueValidator(1)]
+    )
+    description = models.TextField()
+    completed_bonus = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    status = models.CharField(
+        max_length=1,
+        choices=[
+            ("D", "Draft"),
+            ("I", "In Review"),
+            ("P", "Published"),
+            ("R", "Removed"),
+        ],
+        default="D",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(
+        "accounts.Teacher",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="tests_created",
+    )
+
+    def __str__(self):
+        # (Skill - Level) Title (first 30 chars only)
+        return f"({self.skill} - {self.level}) {self.title[:30]}"
+
+    class Meta:
+        db_table = "test"
+
+
+class ReceptiveTest(models.Model):
+    test = models.OneToOneField(Test, on_delete=models.CASCADE, primary_key=True)
+
+    total_score = models.IntegerField(validators=[MinValueValidator(0)], default=0)
+    base_qualified_bonus = models.IntegerField(
+        validators=[MinValueValidator(0)], default=0
+    )
+
+    def __str__(self):
+        return f"Receptive Test for {self.test.title}"
+
+    class Meta:
+        db_table = "receptive_test"
+
+class ProductiveTest(models.Model):
+    test = models.OneToOneField(Test, on_delete=models.CASCADE, primary_key=True)
+
+    format = models.CharField(max_length=1, choices=[]) # Update choices when needed
+    question_text = models.TextField()
+    resources = models.JSONField(default=dict)
+    criterias = models.JSONField(default=dict)
+
+    def __str__(self):
+        return f"Productive Test for {self.test.title}"
+    
+    class Meta:
+        db_table = 'productive_test'
