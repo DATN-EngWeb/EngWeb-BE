496d4791f1aa16a8470579770eb4caa19af7554f
vunebie-pizza4ps <vu.nguyen@pizza4ps.con>
2026-03-04 08:54:29 +0700

    update models

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class TestFeedback(models.Model):
+    test = models.ForeignKey("tests.Test", on_delete=models.CASCADE)
+    teacher = models.ForeignKey(
+        "accounts.Teacher",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(
+        max_length=10,
+        choices=[
+            ("A", "AI"),
+            ("T", "Teacher"),
+        ],
+        default="T",
+    )
+
+    def __str__(self):
+        return f"Feedback for test {self.test_id}"
+
+    class Meta:
+        db_table = "test_feedback"
