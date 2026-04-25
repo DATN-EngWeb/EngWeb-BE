dbad9d224cb36069dae76954adb94354581afa8f
Daniiislo <22120383@student.hcmus.edu.vn>
2026-04-25 21:11:08 +0700

    chore(user-progress): update streak_reward_rule seed

--- init/seed_00_streak_reward_rule.sql
+++ init/seed_00_streak_reward_rule.sql
@@ -3,9 +3,9 @@
 
 INSERT INTO streak_reward_rule (streak_day, xp_reward, ai_turn_reward)
 VALUES
-(3, 20, 2),
-(5, 50, 3),
-(10, 100, 5),
+(3, 20, 1),
+(7, 50, 3),
+(14, 100, 5),
 (30, 200, 10),
 (100, 400, 15)
 ON CONFLICT (streak_day) DO NOTHING;
